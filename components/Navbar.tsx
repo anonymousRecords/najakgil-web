d6debc498d34cf762dad48bf6ab02e9e15323c0f
anonymousrecords <a.Records2022@gmail.com>
2024-05-30 14:33:55 +0900

    feat : zIndex 추가

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   const searchParams = usePathname();
 
   return (
-    <nav className="fixed right-0 top-0 w-full bg-[#2294FF] py-8 md:px-20">
+    <nav className="fixed right-0 top-0 w-full bg-[#2294FF] py-8 md:px-20" style={{zIndex: "10"}}>
       <div className="sm: prose prose-xl flex items-center justify-end gap-10 px-8 text-lg text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
         {navElements.map((element) => (
           <Link
