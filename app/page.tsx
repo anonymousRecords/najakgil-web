594c2c407a1f47931bf361d0b5da2236370cc837
anonymousrecords <a.Records2022@gmail.com>
2024-05-30 14:34:14 +0900

    feat : 캐릭터 이미지 눈송이처럼 떨어지는 애니메이션 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,76 @@
-export default function Home() {
+"use client";
+import { useEffect } from "react";
+import gsap from "gsap";
+import Image from "next/image";
+import { CharacterElement } from "@/public";
+
+// 생성할 눈송이 개수
+const SNOWFLAKE_COUNT = 15;
+
+const Home = () => {
+  useEffect(() => {
+    const snowflakes = document.querySelectorAll(".snowflake");
+    snowflakes.forEach((snowflake) => {
+      animateSnowflake(snowflake);
+    });
+  }, []);
+
+  const animateSnowflake = (snowflake: Element) => {
+    // 눈송이가 화면을 가로지르는데 걸리는 시간
+    const duration = Math.random() * 10 + 5;
+    // 눈송이가 화면 위에서 아래로 떨어지는데 걸리는 시간
+    const fallDuration = Math.random() * 8 + 5; 
+    // 눈송이가 화면에 처음 등장하는 시간을 결정
+    const delay = Math.random() * -15; 
+
+    gsap.to(snowflake, {
+      duration,
+      x: `+=${Math.random() * 400 - 200}`,
+      repeat: -1,
+      yoyo: true,
+      ease: "sine.inOut",
+    });
+    gsap.to(snowflake, {
+      duration: fallDuration,
+      y: window.innerHeight + 100,
+      ease: "none",
+      repeat: -1,
+      delay,
+    });
+    gsap.fromTo(
+      snowflake,
+      { opacity: 0.2, duration: 1 },
+      { opacity: 1, duration: 1 },
+    );
+  };
+
+  const characterElements = Object.values(CharacterElement);
+
+  const snowflakes = Array.from({ length: SNOWFLAKE_COUNT }).map((_, index) => {
+    // 사이즈 랜덤하게 설정
+    const size = Math.random() * 500 + 100;
+    return (
+      <Image
+        key={index}
+        className="snowflake absolute"
+        src={characterElements[index % characterElements.length]}
+        alt={`snowflake-${index}`}
+        style={{
+          pointerEvents: 'none',
+          // 랜덤한 위치에 눈송이 생성
+          left: `${Math.random() * 100}vw`,
+          top: `${Math.random() * -600}px`,
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+        width={size}
+        height={size}
+      />
+    );
+  });
+
   return (
-    <main>
-      dd
-    </main>
+    <div className="relative h-full w-screen overflow-hidden">{snowflakes}</div>
   );
-}
+};
+export default Home;
--- public/index.ts
+++ public/index.ts
@@ -18,7 +18,7 @@ export const GoodsElement = [
       "흩날리는 벚꽆 아래 바람개비 동산에서 봄날을 만끽해보세요! 어쩌면 쿠키를 만날지도",
     category: "데스크탑",
     downloadUrl:
-    "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
+      "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
   },
   {
     id: "3",
@@ -28,7 +28,7 @@ export const GoodsElement = [
       "“우리 아기들 날 찾아보게!” 튤립 속 미니길여를 찾아보세요 찾는 사람에게는 미니길여가 A+을 빌어 준대요..!",
     category: "데스크탑",
     downloadUrl:
-    "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
+      "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
   },
   {
     id: "4",
@@ -38,6 +38,37 @@ export const GoodsElement = [
       "다가오는 여름, 꿉꿉한 장마 다가오는 여름, 꿉꿉한 장마 다가오는 여름, 꿉꿉한 장마 다가오는 여름, 꿉꿉한 장마",
     category: "데스크탑",
     downloadUrl:
-    "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
+      "https://drive.google.com/file/d/1keLqqLIb86PyIB4qJLCpaO1omODupaxl/view",
   },
 ];
+
+export const CharacterElement = {
+  fhanbok_1: "/character/fhanbok-1.png",
+  fhanbok_2: "/character/fhanbok-2.png",
+  fhanbok_3: "/character/fhanbok-3.png",
+  fhanbok_4: "/character/fhanbok-4.png",
+  graduationsuit_1: "/character/graduationsuit-1.png",
+  graduationsuit_2: "/character/graduationsuit-2.png",
+  graduationsuit_3: "/character/graduationsuit-3.png",
+  graduationsuit_4: "/character/graduationsuit-4.png",
+  mhanbok_1: "/character/mhanbok-1.png",
+  mhanbok_2: "/character/mhanbok-2.png",
+  mhanbok_3: "/character/mhanbok-3.png",
+  mhanbok_4: "/character/mhanbok-4.png",
+  scrub_1: "/character/scrub-1.png",
+  scrub_2: "/character/scrub-2.png",
+  scrub_3: "/character/scrub-3.png",
+  scrub_4: "/character/scrub-4.png",
+  suit_1: "/character/suit-1.png",
+  suit_2: "/character/suit-2.png",
+  suit_3: "/character/suit-3.png",
+  suit_4: "/character/suit-4.png",
+  summer_1: "/character/summer-1.png",
+  summer_2: "/character/summer-2.png",
+  summer_3: "/character/summer-3.png",
+  summer_4: "/character/summer-4.png",
+  winter_1: "/character/winter-1.png",
+  winter_2: "/character/winter-2.png",
+  winter_3: "/character/winter-3.png",
+  winter_4: "/character/winter-4.png",
+};
