0ac608b4082c559b047336d083d3f773cbf49706
anonymousrecords <a.Records2022@gmail.com>
2024-06-05 14:56:35 +0900

    fix : ListItem 여백 수정

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -11,7 +11,7 @@ export default function ListItem({ post }: ListItemProps) {
   const { id, title, description, category } = post;
   return (
     <Link
-      className="hover: mx-auto flex flex-col justify-between space-y-4 rounded-xl border-2 bg-white p-8 transition-all duration-300 ease-in-out hover:-translate-y-1.5 md:mx-20 md:flex-row md:space-y-0"
+      className="hover: flex flex-col justify-between space-y-4 rounded-xl border-2 bg-white p-8 transition-all duration-300 ease-in-out hover:-translate-y-1.5 md:mx-20 md:flex-row md:space-y-0"
       href={`/posts/${id}`}
     >
       <div className="flex flex-1 flex-col">
